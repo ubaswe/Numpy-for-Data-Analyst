557b761ceccde79170360f8420702c81173da094
Ubaswe <127177765+ubaswe@users.noreply.github.com>
2023-12-27 02:09:21 +0600

    Add files via upload

---
+++ Array.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
+#zeros matrix
+np.zeros((2,3))
+
+
+# In[29]:
+
+
+#one 
+np.ones((4,2,),dtype="int32")
+
+
+# In[30]:
+
+
+#full Matrix
+np.full((5,6),99)
+
+
+# In[31]:
+
+
+#rand
+np.random.rand(4,2)
+
+
+# In[45]:
+
+
+#randint
+print(np.random.randint(1,100,10))
+
+
+# In[46]:
+
+
+#randint
+np.random.randint(-4,8,size=(3,3))
+
+
+# In[38]:
+
+
+#randn
+np.random.randn(20)
+
+
+# In[49]:
+
+
+np.random.randn(30)
+
