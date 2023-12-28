91915bfcd8025722ccbdb288776bb37d8b7a964c
Ubaswe <127177765+ubaswe@users.noreply.github.com>
2023-12-29 00:20:03 +0600

    Arange,linspace,1D,2D,3D.py

--- Arange-1D,2D,3D.py
+++ Arange-1D,2D,3D.py
@@ -1,28 +1,29 @@
 
 #arange
 import numpy as np
+#0 to 100
 np.arange(100)
 
 
-# In[13]:
-
-
+# In[13]:lower limit 3 upper limit 10 and gap 2.
 np.arange(3,10,2)
 
 
 # In[4]:
-
-
 np.arange(1,100,5)
 
-
 # In[14]:
-
-
 np.arange(2,90,2 ,dtype=float)
 
 
-# In[38]:
+# In[14]:linspace function lower limit 1 upper limit 10 and 100 means divion
+np.linspace(1,10,100)
+
+np.linspace(1,10,100,)
+
+# In[38]:lower limit 1 upper limit 14 and 100 pieces,retstep=True will show the differance of piece
+
+np.linspace(1,15,100,retstep=True)
 
 
 #1-D array
